0e3e414eb56f7175c3c11ae252d145b5130e50ff
jathavaan <jathavaan12@gmail.com>
2026-09-22 10:41:11 +0200

    Installed todo-comments.nvim

---
+++ lua/plugins/todo-comments.lua
@@ -0,0 +1,24 @@
+return {
+	"folke/todo-comments.nvim",
+	event = { "BufReadPost", "BufNewFile" },
+	dependencies = { "nvim-lua/plenary.nvim" },
+	opts = {},
+	keys = {
+		{
+			"]t",
+			function()
+				require("todo-comments").jump_next()
+			end,
+			desc = "Next todo comment",
+		},
+		{
+			"[t",
+			function()
+				require("todo-comments").jump_prev()
+			end,
+			desc = "Prev todo comment",
+		},
+		{ "<leader>ft", "<cmd>TodoTelescope<cr>", desc = "Find todos (project)" },
+		{ "<leader>xt", "<cmd>Trouble todo toggle<cr>", desc = "Todos (Trouble)" },
+	},
+}
