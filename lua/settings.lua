9481f6169365a2ac8990a748fd9d9001fadcb1fb
jathavaan <jathavaan12@gmail.com>
2026-09-22 10:43:25 +0200

    Removed deprecated settings and added some minor styling changes

--- lua/settings.lua
+++ lua/settings.lua
@@ -1,45 +1,47 @@
-local global = vim.g
 local o = vim.o
 local api = vim.api
 
-vim.scriptencoding = "uft-8"
-
--- Map leader
-global.mapleader = " "
-global.maplocalleader = " "
+-- Leader (must be set before lazy.nvim loads plugins)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 -- Editor options
 o.number = true
 o.relativenumber = true
 o.clipboard = "unnamedplus"
-o.syntax = "on"
-o.autoindent = true
 o.expandtab = true
 o.shiftwidth = 4
 o.tabstop = 4
-o.encoding = "utf-8"
-o.fileencoding = "utf-8"
-o.ruler = true
 o.mouse = "a"
 o.title = true
-o.hidden = true
 o.ttimeoutlen = 0
-o.wildmenu = true
-o.showcmd = true
 o.showmatch = true
 o.inccommand = "split"
 o.termguicolors = false
+o.signcolumn = "yes"
 
+-- Add filetypes for Docker compose
+vim.filetype.add({
+	pattern = {
+		["compose.*%.ya?ml"] = "yaml.docker-compose",
+		["docker%-compose.*%.ya?ml"] = "yaml.docker-compose",
+	},
+})
+
+-- Yank highlight
 local function set_yank_hl()
-  api.nvim_set_hl(0, "YankHighlight", { link = "Visual" })
+	api.nvim_set_hl(0, "YankHighlight", { link = "Visual" })
 end
 
 set_yank_hl()
-api.nvim_create_autocmd("ColorScheme", { callback = set_yank_hl })
+
+local yank_group = api.nvim_create_augroup("YankHighlight", { clear = true })
+
+api.nvim_create_autocmd("ColorScheme", { group = yank_group, callback = set_yank_hl })
 
 api.nvim_create_autocmd("TextYankPost", {
-  callback = function()
-    local hl = vim.hl or vim.highlight
-    hl.on_yank({ higroup = "YankHighlight", timeout = 300 })
-  end,
+	group = yank_group,
+	callback = function()
+		vim.hl.on_yank({ higroup = "YankHighlight", timeout = 300 })
+	end,
 })
